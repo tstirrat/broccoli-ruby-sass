0475fc86d60a36dd763eeed6e63422ef837316d2
Johnny Robeson <johnny@localmomentum.net>
2014-07-17 06:15:44 -0400

    Replace quickTemp with broccoli-writer
    Inspired by this change:
    https://github.com/joliss/broccoli-sass/commit/a1c305f473e631af1b60ef4925f8e55d25910e15

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var fs = require('fs');
 var path = require('path');
 var mkdirp = require('mkdirp');
 var includePathSearcher = require('include-path-searcher');
-var quickTemp = require('quick-temp');
+var Writer = require('broccoli-writer');
 var mapSeries = require('promise-map-series');
 var _ = require('lodash');
 var dargs = require('dargs');
@@ -10,6 +10,9 @@ var spawn = require('win-spawn');
 var Promise = require('rsvp').Promise;
 
 module.exports = SassCompiler;
+SassCompiler.prototype = Object.create(Writer.prototype);
+SassCompiler.prototype.constructor = SassCompiler;
+
 function SassCompiler (sourceTrees, inputFile, outputFile, options) {
   if (!(this instanceof SassCompiler)) return new SassCompiler(sourceTrees, inputFile, outputFile, options);
   this.sourceTrees = sourceTrees;
@@ -27,11 +30,10 @@ function SassCompiler (sourceTrees, inputFile, outputFile, options) {
   };
 }
 
-SassCompiler.prototype.read = function (readTree) {
+SassCompiler.prototype.write = function (readTree, destDir) {
   var self = this;
   var bundleExec = this.sassOptions.bundleExec;
-  quickTemp.makeOrRemake(this, '_tmpDestDir');
-  var destFile = this._tmpDestDir + '/' + this.outputFile;
+  var destFile = destDir + '/' + this.outputFile;
   mkdirp.sync(path.dirname(destFile));
   return mapSeries(this.sourceTrees, readTree)
     .then(function (includePaths) {
@@ -100,14 +102,8 @@ SassCompiler.prototype.read = function (readTree) {
             reject('broccoli-ruby-sass exited with error code ' + code);
           }
 
-          resolve(self._tmpDestDir);
+          resolve();
         });
-
-        return self._tmpDestDir;
       });
     });
-};
-
-SassCompiler.prototype.cleanup = function () {
-  quickTemp.remove(this, '_tmpDestDir');
 };
